23956eaea804b1657f8b55f9063cbb0a5fd76375
rheasenan <58535170+rheasenan@users.noreply.github.com>
2019-12-04 19:07:42 -0600

    Update MoveBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveBot.java
@@ -7,4 +7,15 @@ public Movement(HardwareRobot arobot) {
 robot = arobot;
 }
 
+  public double motorPower(double speed) {
+    double power = 1*speed;
+    return power;
+  }
+ 
+  public void drive (double power, double time) {
+   leftDrive.setPower(.power);
+   rightDrive.setPower(-power);
+   sleep(time);
+ }
+  
 }
